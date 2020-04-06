2f3fc956797ba96b393d8c0e4e92256f18adbac9
gamzeguven <gamze.guven@bil.omu.edu.tr>
2020-04-06 18:54:53 +0300

    Initial Commit

---
+++ MarsRoverTask/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MarsRoverTask
+//
+//  Created by Gamze Güven on 6.04.2020.
+//  Copyright © 2020 Gamze Güven. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
